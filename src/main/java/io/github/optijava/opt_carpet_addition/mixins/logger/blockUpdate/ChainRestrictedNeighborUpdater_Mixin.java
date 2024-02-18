a8d5d5e47799f543fab509a71e89c00e294704a7
OptiJava <OptiJava@protonmail.com>
2024-02-18 21:56:13 +0800

    feat: finished logger blockUpdate at ChainRestrictedNeighborUpdater in 1.19 & 1.20

--- src/main/java/io/github/optijava/opt_carpet_addition/mixins/logger/blockUpdate/ChainRestrictedNeighborUpdater_Mixin.java
+++ src/main/java/io/github/optijava/opt_carpet_addition/mixins/logger/blockUpdate/ChainRestrictedNeighborUpdater_Mixin.java
@@ -1,6 +1,9 @@
 package io.github.optijava.opt_carpet_addition.mixins.logger.blockUpdate;
 
 //#if MC >= 11900
+//$$ import carpet.CarpetServer;
+//$$ import io.github.optijava.opt_carpet_addition.logger.blockUpdate.BlockUpdateLogger;
+//$$ import net.minecraft.block.Block;
 //$$ import net.minecraft.util.math.BlockPos;
 //$$ import net.minecraft.world.block.ChainRestrictedNeighborUpdater;
 //$$ import org.spongepowered.asm.mixin.Mixin;
@@ -15,8 +18,24 @@ package io.github.optijava.opt_carpet_addition.mixins.logger.blockUpdate;
 //$$             method = "enqueue",
 //$$             at = @At("HEAD")
 //$$     )
-//$$     private void injectEnqueue(BlockPos pos, ChainRestrictedNeighborUpdater.Entry entry, CallbackInfo ci) {
-//$$         // TODO: log
+//$$     private void injectEnqueue(BlockPos updatingBlockPos, ChainRestrictedNeighborUpdater.Entry entry, CallbackInfo ci) {
+//$$         Block updatingBlock = CarpetServer.minecraft_server.getCommandSource().getWorld().getChunk(updatingBlockPos).getBlockState(updatingBlockPos).getBlock();
+//$$
+//$$         Block sourceBlock = null;
+//$$         BlockPos centreBlockPos = null;
+//$$
+//$$         if (entry instanceof ChainRestrictedNeighborUpdater.SimpleEntry simpleEntry) {
+//$$             sourceBlock = simpleEntry.sourceBlock();
+//$$             centreBlockPos = simpleEntry.sourcePos();
+//$$         } else if (entry instanceof ChainRestrictedNeighborUpdater.StatefulEntry statefulEntry) {
+//$$             sourceBlock = statefulEntry.sourceBlock();
+//$$             centreBlockPos = statefulEntry.sourcePos();
+//$$         } else if (entry instanceof ChainRestrictedNeighborUpdater.SixWayEntry sixWayEntry) {
+//$$             sourceBlock = sixWayEntry.sourceBlock;
+//$$             centreBlockPos = sixWayEntry.pos;
+//$$         }
+//$$
+//$$         BlockUpdateLogger.INSTANCE.logBlockUpdate(updatingBlock, updatingBlockPos, sourceBlock, centreBlockPos);
 //$$     }
 //$$ }
 //#endif

cada8b6b03c15e190d54d3537a6747365e1b06c7
OptiJava <OptiJava@protonmail.com>
2024-08-05 20:45:40 +0800

    chore: add support for 1.21

--- src/main/java/io/github/optijava/opt_carpet_addition/mixins/rule/removeAllCurseOfBindingArmorWhenPlayerDeadInWall/ServerPlayerEntity_Mixin.java
+++ src/main/java/io/github/optijava/opt_carpet_addition/mixins/rule/removeAllCurseOfBindingArmorWhenPlayerDeadInWall/ServerPlayerEntity_Mixin.java
@@ -28,9 +28,15 @@ public class ServerPlayerEntity_Mixin {
         if (OptCarpetSettings.removeAllCurseOfBindingArmorWhenPlayerDeadInWall && ((ServerPlayerEntity) (Object)this).world.getGameRules().getBoolean(GameRules.KEEP_INVENTORY) && Objects.equals(source.getName(), "inWall")) {
         //#endif
             for (ItemStack armor : ((ServerPlayerEntity) (Object)this).getArmorItems()) {
+                //#if MC >= 12100
+                //$$ if (EnchantmentHelper.hasAnyEnchantmentsWith(armor, net.minecraft.component.EnchantmentEffectComponentTypes.PREVENT_EQUIPMENT_DROP)) {
+                //$$     armor.setCount(0);
+                //$$ }
+                //#else
                 if (EnchantmentHelper.getLevel(Enchantments.BINDING_CURSE, armor) > 0) {
                     armor.setCount(0);
                 }
+                //#endif
             }
         }
     }
